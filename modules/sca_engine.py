e1e9e225057097d79b7ce18dc344c99ff0ec355c
adhityorafael <adhityorafael@gmail.com>
2026-08-28 16:51:02 +0700

    Update sca_engine.py

--- modules/sca_engine.py
+++ modules/sca_engine.py
@@ -196,6 +196,6 @@ def run(df_kontingensi):
     fig.add_vline(x=0, line_color="gray", line_width=1, line_dash="dot")
 
     # Tampilkan di Streamlit
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch")
 
     return sca_result
